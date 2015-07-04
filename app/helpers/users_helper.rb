d54749e322693f4357fe16fc7e85f1b38f836d64
Jordan Storz <jm.storz@gmail.com>
2015-07-04 13:37:38 -0600

    Install pagination, Add admin user attribute, Add index with delete option, Add authorization for delete.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   
   #returns the Gravatar for the given user
-  def gravatar_for(user, options = { size: 200 })
+  def gravatar_for(user, options = { size: 130 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:franklin)
   end
   
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
@@ -38,5 +43,23 @@ class UsersControllerTest < ActionController::TestCase
     assert flash.empty?
     assert_redirected_to root_url
   end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+  
+  
+  
 
 end
